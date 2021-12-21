c3aea942cb7b8e3cd984f2a774dc5144951c7cc7
Elmira Tashvighi <elmira.tashvighi@gmail.com>
2021-12-21 14:36:25 -0800

    update

---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE department (
+    id INT PRIMARY KEY department_id name VARCHAR(30)
+) CREATE role (
+    id INT title VARCHAR(30) salary DECIMAL department_id INT
+) CREATE employee (
+    id INT first_name VARCHAR(30) last_name VARCHAR(30) role_id INT
+)
