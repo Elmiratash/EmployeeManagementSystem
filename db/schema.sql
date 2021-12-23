7ab4cf5704ccd4aa5de017ceb5cacc843956a5fc
Elmira Tashvighi <elmira.tashvighi@gmail.com>
2021-12-22 17:02:07 -0800

    finish db

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,24 @@
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department;
 CREATE TABLE department (
-    id INT PRIMARY KEY department_id name VARCHAR(30)
-) CREATE role (
-    id INT title VARCHAR(30) salary DECIMAL department_id INT
-) CREATE employee (
-    id INT first_name VARCHAR(30) last_name VARCHAR(30) role_id INT
-)
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INTEGER,
+    CONSTRAINT fk_deptID FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE
+    SET NULL
+);
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER,
+    manager_id INTEGER references employee(id) ON update cascade,
+    CONSTRAINT fk_role FOREIGN KEY(role_id) REFERENCES role(id) ON DELETE
+    SET NULL
+);
