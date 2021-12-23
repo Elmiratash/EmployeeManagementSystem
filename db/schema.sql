2ea04a044dea9bf414ce0a06ea0c8860879df561
Elmira Tashvighi <elmira.tashvighi@gmail.com>
2021-12-22 18:14:34 -0800

    try to fix error

--- db/schema.sql
+++ db/schema.sql
@@ -1,35 +1,29 @@
-DROP DATABASE IF EXISTS employee_tracker_db;
-CREATE DATABASE employee_tracker_db;
-USE employee_tracker_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
 CREATE TABLE department (
-    id INT AUTO_INCREMENT NOT NULL,
-    name VARCHAR(30),
-    PRIMARY KEY(id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
 );
 CREATE TABLE role (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
-    PRIMARY KEY(id),
-    FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE
     SET NULL
 );
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
-    PRIMARY KEY(id),
-    FOREIGN KEY(role_id) REFERENCES role(id) ON DELETE
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE
     SET NULL,
-        FOREIGN KEY(manager_id) REFERENCES employee(id) ON DELETE
+        FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE
     SET NULL
-);
-SELECT *
-FROM department;
-SELECT *
-FROM role;
-SELECT *
-FROM employee;
+)
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,49 +1,24 @@
--- Pre-populates department values --
-INSERT INTO department(name)
-VALUES('Accounting and Finance');
-INSERT INTO department(name)
-VALUES('Marketing');
-INSERT INTO department(name)
-VALUES('Human Resource Management');
-INSERT INTO department(name)
-VALUES('Information Technology');
--- Pre-populates role values --
-INSERT INTO role(title, salary, department_id)
-VALUES('Accounting Manager', 80000, 1);
-INSERT INTO role(title, salary, department_id)
-VALUES('Accountant', 60000, 1);
-INSERT INTO role(title, salary, department_id)
-VALUES('Marketing Manager', 100000, 2);
-INSERT INTO role(title, salary, department_id)
-VALUES('Marketing Specialist', 70000, 2);
-INSERT INTO role(title, salary, department_id)
-VALUES('HR Manager', 90000, 3);
-INSERT INTO role(title, salary, department_id)
-VALUES('HR Administrator', 60000, 3);
-INSERT INTO role(title, salary, department_id)
-VALUES('IT Manager', 95000, 4);
-INSERT INTO role(title, salary, department_id)
-VALUES('IT Consultant', 75000, 4);
--- Pre-populates employee values --
-INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES('Samantha', 'Smith', 1, null);
-INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES('Jack', 'Tang', 2, 1);
-INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES('Amelia', 'Thompson', 2, 1);
-INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES('Eliza', 'Wilkinson', 3, null);
-INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES('Brie', 'Singh', 4, 3);
-INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES('Chris', 'Rodriguez', 4, 3);
-INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES('Tyler', 'McHatton', 5, null);
-INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES('Billy', 'Kwon', 6, 5);
-INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES('Mandy', 'Antonov', 7, null);
-INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES('Kai', 'Murasaki', 8, 7);
-INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES('Ben', 'Rossi', 8, 7);
+INSERT INTO department (name)
+VALUES ("Finance"),
+    ("Human Resources"),
+    ("Sales"),
+    ("Animal Control"),
+    ("Engineering"),
+    ("Management");
+INSERT INTO role (title, salary, department_id)
+VALUES ("Head Accounting", 50000, 1),
+    ("Health and Safety Coordinator", 35000, 2),
+    ("HR Manager", 60000, 6),
+    ("Animal Handler", 30000, 4),
+    ("Air Conditioning Engineer", 90000, 5),
+    ("Product Specialist", 45000, 3);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Jamie", "Chu", 6),
+    ("Angela", "Chang", 3),
+    ("Hannah", "Lorayne", 4),
+    ("Alison", "Nguyen", 5);
+UPDATE employee
+SET manager_id = 2
+WHERE id = 1
+    or id = 3
+    or id = 4;
